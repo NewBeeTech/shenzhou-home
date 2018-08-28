046550c840c80ae28f6dd8c3ac510369e2b55474
dearW <1029219450@qq.com>
2018-08-28 15:42:33 +0800

    fix

--- config/config.local.js
+++ config/config.local.js
@@ -2,7 +2,7 @@ const host = '0.0.0.0';
 const APIURL = 'http://118.190.158.183:8080/api/'; // 开发环境
 module.exports = {
   host: host,
-  port: '5000',
+  port: '3003',
   proxyURL: APIURL,
   app: {
     ENV: JSON.stringify('local'),
